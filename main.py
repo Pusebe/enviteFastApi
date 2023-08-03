9045b34bfa47b3451186124ada3cc3241f3b39f9
Pusebe <serbeise@hotmail.com>
2023-08-03 17:47:15 +0100

    modified:   main.py 	modified:   templates/table.html

--- main.py
+++ main.py
@@ -227,4 +227,4 @@ async def websocket_endpoint(websocket: WebSocket, table_id:int):
         new_set = True
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5001)
