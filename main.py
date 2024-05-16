fb965bcf04a77474d3e280201d6d863bb91ce396
Pusebe <serbeise@hotmail.com>
2024-05-16 14:53:42 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -166,7 +166,6 @@ async def websocket_endpoint(websocket: WebSocket, table_id:int):
                                 card_played = player.play_card(card)
                                 game.cards_played.append(card_played)
                                 await manager.broadcast({"player": player.name,"card_played" : card_played.value + card_played.suit})
-                                print(f"el jugador jugó {card_played.value} de {card_played.suit}")
                                 #ya jugaste asi que le mando al cliente que ya no es tu turno
                                 await manager.send_personal_message({"turn": False}, users_connected_to_socket.get(game.players_order[0].name))
                         card = None
@@ -190,9 +189,7 @@ async def websocket_endpoint(websocket: WebSocket, table_id:int):
                        #el que gana la mano le mandamos un mensajito cambiando el turno a true
                         await manager.broadcast({"turn": False})
                         await manager.send_personal_message({"turn": True}, users_connected_to_socket.get(game.players_order[0].name))
-                        print(f"El equipo 1 tiene {game.team1.games_won} chicos.\nY el equipo 2 tiene {game.team2.games_won} chicos.\n")
-                        await manager.broadcast({"chicos": {"team1": game.team1.games_won, "team2":game.team2.games_won} , "piedras": {"team1":game.team1.sets_won, "team2":game.team2.sets_won}})
-                       
+                                      
                     if game.team1.has_won_round(game.points_to_win_round):
                         game.team1.increment_sets_won()
                         print("El equipo 1 uno ganó la ronda y dos piedritas")
@@ -206,7 +203,10 @@ async def websocket_endpoint(websocket: WebSocket, table_id:int):
                         
                     if game.team2.has_won_set(game.points_to_win_set):
                         game.team2.increment_games_won()
-
+                        
+                    print(f"El equipo 1 tiene {game.team1.games_won} chicos.\nY el equipo 2 tiene {game.team2.games_won} chicos.\n")
+                    await manager.broadcast({"chicos": {"team1": game.team1.games_won, "team2":game.team2.games_won} , "piedras": {"team1":game.team1.sets_won, "team2":game.team2.sets_won}})
+         
 
                 else:
                     new_set = True
